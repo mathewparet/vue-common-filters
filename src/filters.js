87f781a20c191a790633ff2f72fc4cb187a5cca9
Mathew Paret <mathewparet@gmail.com>
2020-12-24 22:10:10 +1100

    Removed options for default options globally

--- src/filters.js
+++ src/filters.js
@@ -1,41 +1,22 @@
 import moment from "moment";
 export default {
 
-    options: {
-        defaults: {
-            moment: {
-                current_format: 'YYYY-MM-DDTHH:mm:ssZ',
-                format: 'llll',
-            },
-            trim: {
-                max_length: 15,
-                lead: '...',
-            }
-        }
-    },
-
-    install(Vue, options) {
+    install(Vue, opt) {
         Vue.filter('trim',this.trim);
         Vue.filter('dateFormat',this.dateFormat);
-        this.options = options;
     },
 
-    trim(value, max_length, lead) {
+    trim(value, max_length = 15, lead = '...') {
         if(!value)
             return;
 
-        max_length = max_length || this.options.defaults.trim.max_length;
-        lead = lead || this.options.defaults.trim.lead;
-        
         if(value.length <= max_length)
             return value;
     
         return value.substring(0, max_length-lead.length)+lead;
     },
     
-    dateFormat(value, format, current_format) {
-        current_format = current_format || this.options.defaults.moment.current_format;
-        format = format || this.options.defaults.moment.format;
+    dateFormat(value, format = 'llll', current_format = 'YYYY-MM-DDTHH:mm:ssZ') {
 
         if(!value)
             return;
